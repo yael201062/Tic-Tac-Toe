347dc7cce76f5d303bfc92878f9513f8c21e1699
SharonBron <7sharonbron7@gmail.com>
2024-12-14 14:16:08 +0200

    style fixes

--- app/src/main/java/com/example/tictactoe/theme/Color.kt
+++ app/src/main/java/com/example/tictactoe/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.tictactoe.ui.theme
+package com.example.tictactoe.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/tictactoe/theme/Theme.kt
+++ app/src/main/java/com/example/tictactoe/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.tictactoe.ui.theme
+package com.example.tictactoe.theme
 
 import android.app.Activity
 import android.os.Build
